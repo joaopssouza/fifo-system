362943cea63c2c789eb084f8bf79960def479111
Joao Paulo <joaop0737@gmail.com>
2025-10-19 23:47:10 -0300

    fix: retirando o 'prepared statements'

--- backend/initializers/database.go
+++ backend/initializers/database.go
@@ -14,7 +14,9 @@ var DB *gorm.DB
 func ConnectToDB() {
 	var err error
 	dsn := config.AppConfig.DatabaseURL
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt: false,
+	})
 
 	if err != nil {
 		log.Fatal("Failed to connect to database")
